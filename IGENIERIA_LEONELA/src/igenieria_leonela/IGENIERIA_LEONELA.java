0045355270ac42a468f923d50b10a6170d704861
leonelaureta <leonelaureta@leonelaureta>
2016-08-25 12:57:40 -0500

    commit 3

--- IGENIERIA_LEONELA/src/igenieria_leonela/IGENIERIA_LEONELA.java
+++ IGENIERIA_LEONELA/src/igenieria_leonela/IGENIERIA_LEONELA.java
@@ -24,6 +24,19 @@ public class IGENIERIA_LEONELA {
             System.out.print("Introduce numero mayor que 1: ");
             numero = sc.nextInt();
         }while(numero<=1);
+        
+        System.out.println("Los " + numero + " primeros términos de la serie de Fibonacci son:"); 
+
+        fibo1=1;
+        fibo2=1; 
+
+        System.out.print(fibo1 + " ");
+        for(i=2;i<=numero;i++){
+             System.out.print(fibo2 + " ");
+             fibo2 = fibo1 + fibo2;
+             fibo1 = fibo2 - fibo1;
+        }
+        System.out.println();
     }
             
        
